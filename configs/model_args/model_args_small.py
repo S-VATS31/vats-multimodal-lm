94bd9dd00ef63427f8c5846188112046d015c7df
Shaan Vats <shaanv3121@gmail.com>
2025-07-12 14:11:05 -0700

    Update configs with correct parameter counts

--- configs/model_args/model_args_small.py
+++ configs/model_args/model_args_small.py
@@ -1,33 +1,18 @@
 """
 Small configuration of model arguments.
 
-This configuration contains ~379 million parameters.
+This configuration contains:
+    - 379 million parameters.
 
-Note that the training arguments will need to updated 
-based on different ModelArgs parameter sizes for better
-training and convergence.
+This configuration is too small for MoE, set: top_k, num_experts = 1, 1.
 """
+
 
 from dataclasses import dataclass
 
 @dataclass
 class ModelArgs:
-    """
-    d_model (int): Dimensionality of the model's embeddings.
-    num_heads (int): Number of attention heads for the query tensors (GQA).
-    query_groups (int): Number of query groups for the key and value tensors (GQA).
-    d_ffn (int): Dimensionality of the feed forward network. d_ffn = 4 * d_model.
-    num_layers (int): Number of times the transformer block will be stacked.
-    dropout (float): Probability of random model components being dropped out.
-    rope_base (float): Exponential base of the RoPE inverse frequency.
-    rms_norm_eps (float): Small epsilon value to ensure numerical stability in RMSNorm.
-    vocab_size (int): Number of unique tokens the model can recognize.
-    max_seq_len (int): Longest input sequence the model can handle at once.
-    tie_weights (bool): Flag to tie weights or not.
-    pad_token_id (int): Number reserved for the padding token which will be masked out.
-    eos_token_id (int): End of sequence token id which is typically vocab_size - 1.
-    gradient_checkpointing (bool): Flag to apply gradient checkpointing or not.
-    """
+    """Small configuration of model arguments."""
     d_model: int = 512
     num_heads: int = 8
     query_groups: int = 4
@@ -41,6 +26,9 @@ class ModelArgs:
     tie_weights: bool = False
     pad_token_id: int = 0
     eos_token_id: int = 65535
+    max_batch_size: int = 2048
     gradient_checkpointing: bool = True
     num_experts: int = 8
     top_k: int = 2
+
+    # TODO: add post_init for assertions
--- configs/model_args/model_args_xlarge.py
+++ configs/model_args/model_args_xlarge.py
@@ -1,37 +1,18 @@
 """
 Extra large configuration of model arguments.
 
-This configuration contains ~13 billion parameters.
-NOTE: This configuration has been not been tested due to the
-computational power needed. This is just a rough estimate based
-on the formula:
- - Total parameters ≈ L * (4 * d_model^2 + 2 * d_model * d_ffn) + 2 * d_model * V
+This configuration contains:
+    - X billion parameters with MoE
+    - Y billion parameters without MoE
 
-Note that the training arguments will need to updated 
-based on different ModelArgs parameter sizes for better
-training and convergence.
+To "turn off" MoE, set num_experts=1 and top_k=1.
 """
 
 from dataclasses import dataclass
 
 @dataclass
 class ModelArgs:
-    """
-    d_model (int): Dimensionality of the model's embeddings.
-    num_heads (int): Number of attention heads for the query tensors (GQA).
-    query_groups (int): Number of query groups for the key and value tensors (GQA).
-    d_ffn (int): Dimensionality of the feed forward network. d_ffn = 4 * d_model.
-    num_layers (int): Number of times the transformer block will be stacked.
-    dropout (float): Probability of random model components being dropped out.
-    rope_base (float): Exponential base of the RoPE inverse frequency.
-    rms_norm_eps (float): Small epsilon value to ensure numerical stability in RMSNorm.
-    vocab_size (int): Number of unique tokens the model can recognize.
-    max_seq_len (int): Longest input sequence the model can handle at once.
-    tie_weights (bool): Flag to tie weights or not.
-    pad_token_id (int): Number reserved for the padding token which will be masked out.
-    eos_token_id (int): End of sequence token id which is typically vocab_size - 1.
-    gradient_checkpointing (bool): Flag to apply gradient checkpointing or not.
-    """
+    """Extra large configuration of model arguments."""
     d_model: int = 5120
     num_heads: int = 40
     query_groups: int = 10
