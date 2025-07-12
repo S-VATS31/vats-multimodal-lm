10f7297d5e1c7c76dc812cb62e5c80038a91c1ab
Shaan Vats <shaanv3121@gmail.com>
2025-07-12 14:20:27 -0700

    Added post initialization for assertions

--- configs/model_args/model_args_small.py
+++ configs/model_args/model_args_small.py
@@ -10,6 +10,9 @@ This configuration is too small for MoE, set: top_k, num_experts = 1, 1.
 
 from dataclasses import dataclass
 
+from configs.training_args import TrainingArgs
+training_args = TrainingArgs
+
 @dataclass
 class ModelArgs:
     """Small configuration of model arguments."""
@@ -31,4 +34,15 @@ class ModelArgs:
     num_experts: int = 8
     top_k: int = 2
 
-    # TODO: add post_init for assertions
+    def __post_init__(self):
+        """Post initialization for assertions."""
+        if self.d_model % self.num_heads != 0:
+            raise ValueError(f"d_model must be divisble by 0, got {self.d_model} / {self.num_heads} != 0")
+        if self.num_heads % self.query_groups != 0:
+            raise ValueError(f"d_model must be divisble by 0, got {self.num_heads} / {self.query_groups} != 0")
+        if self.d_model * 4 != self.d_ffn:
+            raise ValueError(f"d_model * 4 must be equal to d_ffn, got {self.d_model} * 4 != {self.d_ffn}")
+        if self.max_batch_size < training_args.batch_size:
+            raise ValueError(f"max_batch_size must be >= batch_size, got {training_args.batch_size} < {self.max_batch_size}")
+        if self.num_experts < self.top_k:
+            raise ValueError(f"num_experts must be >= top_k, got {self.top_k} > {self.num_experts}")
