968ebbe62a9ec9cdad415043116e6deb3e14f2e7
Shaan Vats <shaanv3121@gmail.com>
2025-07-13 01:03:35 -0700

    added post init for assertions

--- configs/model_args/model_args_xsmall.py
+++ configs/model_args/model_args_xsmall.py
@@ -9,6 +9,9 @@ This configuration is too small for MoE, set: top_k, num_experts = 1, 1.
 
 from dataclasses import dataclass
 
+from configs.training_args import TrainingArgs
+training_args = TrainingArgs()
+
 @dataclass
 class ModelArgs:
     """Extra small configuration of model arguments."""
@@ -29,5 +32,16 @@ class ModelArgs:
     gradient_checkpointing: bool = True
     num_experts: int = 4
     top_k: int = 2
-    
-    # TODO: Add post_init for assertions
+
+    def __post_init__(self):
+        """Post initialization for assertions."""
+        if self.d_model % self.num_heads != 0:
+            raise ValueError(f"d_model must be divisble by 0, got {self.d_model} / {self.num_heads} != 0")
+        if self.num_heads % self.query_groups != 0:
+            raise ValueError(f"d_model must be divisble by 0, got {self.num_heads} / {self.query_groups} != 0")
+        if self.d_model * 4 != self.d_ffn:
+            raise ValueError(f"d_model * 4 must be equal to d_ffn, got {self.d_model} * 4 != {self.d_ffn}")
+        if self.max_batch_size < training_args.batch_size:
+            raise ValueError(f"max_batch_size must be >= batch_size, got {training_args.batch_size} < {self.max_batch_size}")
+        if self.num_experts < self.top_k:
+            raise ValueError(f"num_experts must be >= top_k, got {self.top_k} > {self.num_experts}")
