628c7335a96f2a1ee4dc569ed67a1dc628e467a4
Shaan Vats <shaanv3121@gmail.com>
2025-07-14 23:12:36 -0700

    Fixed import paths for current tests

--- tests/rms_norm_tests.py
+++ tests/rms_norm_tests.py
@@ -5,7 +5,7 @@ from configs.setup_env import device, dtype
 from configs.training_args import TrainingArgs
 from configs.model_args.model_args_medium import ModelArgs
 
-from src.model import RMSNorm
+from src.text_generation.model import RMSNorm
 
 model_args = ModelArgs()
 train_args = TrainingArgs()
