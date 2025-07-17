8ab647647d21ba2ce9cd15e211f896bf4b174f97
Shaan Vats <shaanv3121@gmail.com>
2025-07-17 11:16:32 -0700

    renamed file

---
+++ configs/nlp_transformer/model_args/model_args_medium.py
@@ -0,0 +1,47 @@
+"""
+Medium configuration of model arguments.
+
+This configuration contains:
+    - 811 million parameters.
+
+This configuration is too small for MoE, set: top_k, num_experts = 1, 1.
+"""
+
+from dataclasses import dataclass
+
+from configs.training_args import TrainingArgs
+training_args = TrainingArgs()
+
+@dataclass
+class ModelArgs:
+    """Medium configuration of model arguments."""
+    d_model: int = 1440
+    num_heads: int = 24
+    query_groups: int = 12
+    d_ffn: int = 5760
+    num_layers: int = 20
+    dropout: float = 0.2
+    rope_base: float = 10000.0
+    rms_norm_eps: float = 1e-7
+    vocab_size: int = 65536
+    max_seq_len: int = 2048
+    tie_weights: bool = False
+    pad_token_id: int = 0
+    eos_token_id: int = 65535
+    gradient_checkpointing: bool = True
+    max_batch_size: int = 1024
+    num_experts: int = 16
+    top_k: int = 2
+
+    def __post_init__(self):
+        """Post initialization for assertions."""
+        if self.d_model % self.num_heads != 0:
+            raise ValueError(f"d_model must be divisble by 0, got {self.d_model} / {self.num_heads} != 0")
+        if self.num_heads % self.query_groups != 0:
+            raise ValueError(f"d_model must be divisble by 0, got {self.num_heads} / {self.query_groups} != 0")
+        if self.d_model * 4 != self.d_ffn:
+            raise ValueError(f"d_model * 4 must be equal to d_ffn, got {self.d_model} * 4 != {self.d_ffn}")
+        if self.max_batch_size < training_args.batch_size:
+            raise ValueError(f"max_batch_size must be >= batch_size, got {training_args.batch_size} < {self.max_batch_size}")
+        if self.num_experts < self.top_k:
+            raise ValueError(f"num_experts must be >= top_k, got {self.top_k} > {self.num_experts}")
---
+++ configs/nlp_transformer/training_args.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+from dataclasses import dataclass
+
+@dataclass
+class TrainingArgs:
+    """Dataclass containing model training arguments"""
+    learning_rate: float = 2e-4
+    epochs: int = 3
+    batch_size: int = 256
+    epsilon: float = 1e-6
+    clip_grad_norm: float = 1.0
+    weight_decay: float = 5e-4
+    betas: Tuple[float, float] = (0.9, 0.95)
+    warmup_ratio: float = 0.05
+    aux_loss_weight: float = 0.01
+    eta_min: float = 6e-7
+    num_workers: int = 8
+    pin_memory: bool = True
+    persistent_workers: bool = True
+    drop_last: bool = True
+    train_ratio: float = 0.95
+    grad_accum_steps: int = 4
+    logging_steps: int = 100
+    eval_steps: int = 500
+    save_steps: int = 500
+    save_freq: int = 1
+    max_eval_batches: int = 250
