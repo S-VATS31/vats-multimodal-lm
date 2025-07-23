ebd8f49305d7eaf9dd3f36ca36190f4f4d8bde17
Shaan Vats <shaanv3121@gmail.com>
2025-07-22 21:14:40 -0700

    added post init in all model args

--- configs/transformers/nlp/model_args/model_args_large.py
+++ configs/transformers/nlp/model_args/model_args_large.py
@@ -34,12 +34,19 @@ class ModelArgs:
     def __post_init__(self):
         """Post initialization for assertions."""
         if self.d_model % self.num_heads != 0:
-            raise ValueError(f"d_model must be divisble by 0, got {self.d_model} % {self.num_heads} != 0")
+            raise ValueError(f"Expected d_model to be divisble by num_heads, got {self.d_model} % {self.num_heads} != 0")
         if self.num_heads % self.query_groups != 0:
-            raise ValueError(f"d_model must be divisble by 0, got {self.num_heads} % {self.query_groups} != 0")
+            raise ValueError(f"Expected d_model to be divisble by num_heads, got {self.num_heads} % {self.query_groups} != 0")
         if self.d_model * 4 != self.d_ffn:
-            raise ValueError(f"d_model * 4 must be equal to d_ffn, got {self.d_model} * 4 != {self.d_ffn}")
+            raise ValueError(f"Expected d_model * 4 = d_ffn, got {self.d_model} * 4 != {self.d_ffn}")
         if self.max_batch_size < training_args.batch_size:
-            raise ValueError(f"max_batch_size must be >= batch_size, got {training_args.batch_size} < {self.max_batch_size}")
+            raise ValueError(f"Expected max_batch_size >= batch_size, got {training_args.batch_size} < {self.max_batch_size}")
         if self.num_experts < self.top_k:
-            raise ValueError(f"num_experts must be >= top_k, got {self.top_k} > {self.num_experts}")
+            raise ValueError(f"Expected num_experts >= top_k, got {self.top_k} > {self.num_experts}")
+        if len(self.window_size) != 2:
+            raise ValueError(f"Expected len(window_size) == 2, got {len(self.window_size)} != 2")
+        if self.window_size[0] <= 0:
+            raise ValueError(f"Expected window_size[0] > 0, got {self.window_size[0]} <= 0")
+        if self.window_size[1] != 0:
+            raise ValueError(f"Expected window_size[1] == 0, got {self.window_size[1]} != 0")
+        
