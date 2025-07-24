d4390bf8f7c88b38b7a652717bfc04d2b7c89e37
Shaan Vats <shaanv3121@gmail.com>
2025-07-24 00:04:02 -0700

    added model args for 3d vit, update once model complete

---
+++ configs/transformers/vision/vit_3d/model_args/model_args_xsmall.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+# TODO: Update hyperparameter values once model is complete.
+# TODO: Calculate parameters once model is complete.
+
+@dataclass
+class ModelArgs:
+    """Medium configuration of model arguments, containing X million parameters."""
+    patch_size: Tuple[int, int, int] = (2, 16, 16)
+    C_in: int = 3
+    d_model: int = 1152
+    num_heads: int = 16
+    query_groups: int = 8
+    d_ffn: int = 4608
+    num_layers: int = 16
+    window_size: Tuple[int, int] = (384, 384)
+    dropout: float = 0.2
+    rope_theta: float = 30000.0
+    rms_norm_eps: float = 1e-7
+    num_classes: int = 1000
+    use_checkpointing: bool = True
+
+    def __post_init__(self):
+        """Post initialization for assertions."""
+        if self.d_model % self.num_heads != 0:
+            raise ValueError(f"Expected d_model to be divible by num_heads, got {self.d_model} % {self.num_heads} != 0.")
+        if self.num_heads % self.query_groups != 0:
+            raise ValueError(f"Expected num_heads to be divible by query_groups, got {self.num_heads} % {self.query_groups} != 0.")
+        if self.d_model * 4 != self.d_ffn:
+            raise ValueError(f"Expected d_ffn = d_model * 4, got {self.d_model} * 4 != {self.d_ffn}")
+        if len(self.window_size) != 2:
+            raise ValueError(f"Expected len(window_size) to be equal to 2, got {len(self.window_size)}")
+        if self.window_size[0] != self.window_size[1]:
+            raise ValueError(f"Expected left and right windows to be equal, got {self.window_size[0]} != {self.window_size[1]}")
