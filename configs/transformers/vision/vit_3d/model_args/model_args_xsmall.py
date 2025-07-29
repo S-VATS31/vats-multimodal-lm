1bf1fd8fe40e797cd45bec4afb8a60a8c89dec4d
Shaan Vats <shaanv3121@gmail.com>
2025-07-29 00:31:39 -0700

    removed grid size from all model args, computed dynamically in model.py

--- configs/transformers/vision/vit_3d/model_args/model_args_xsmall.py
+++ configs/transformers/vision/vit_3d/model_args/model_args_xsmall.py
@@ -5,7 +5,6 @@ from typing import Tuple
 class ModelArgs:
     """Extra small configuration of model arguments, containing 241 million parameters."""
     patch_size: Tuple[int, int, int] = (2, 16, 16)
-    grid_size: Tuple[int, int, int] = (2, 2, 2)
     C_in: int = 3
     d_model: int = 1152
     num_heads: int = 16
