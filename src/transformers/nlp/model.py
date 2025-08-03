85a08c97f358e9eaabce53f058b1e1945dc03b48
Shaan Vats <shaanv3121@gmail.com>
2025-08-03 14:03:30 -0700

    imported rms norm/swiglu for modularity

--- src/transformers/nlp/model.py
+++ src/transformers/nlp/model.py
@@ -3,8 +3,6 @@ from configs.setup_env import (
     dtype, 
     use_flash_attn, 
     flash_attn_varlen_qkvpacked_func,
-    use_xformers_swiglu,
-    swiglu
 )
 
 import math
@@ -16,6 +14,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.checkpoint import checkpoint
 
+from src.rms_norm import RMSNorm
+from src.swiglu_activation import SwiGLUActivation
 from configs.transformers.nlp.model_args.model_args_medium import ModelArgs
 
 class RoPE(nn.Module):
@@ -167,38 +167,6 @@ class RoPE(nn.Module):
         cos_freqs = self.cos_cache[:seq_len]
         sin_freqs = self.sin_cache[:seq_len]
         return cos_freqs, sin_freqs
-
-
-class RMSNorm(nn.Module):
-    """Apply RMSNorm to input tensors to normalize their root mean square norm.
-
-    Formula:
-        x_normalized = x / RMS
-        Where RMS = sqrt(mean(x**2))
-
-    Args:
-        d_model (int): Dimensionality of the model's embeddings.
-        eps (float): Small epsilon value to ensure numerical stability.
-    """
-    def __init__(self, d_model: int, eps: float):
-        super().__init__()
-
-        self.eps = eps
-        self.weight = nn.Parameter(torch.ones(d_model)).to(device)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Perform forward pass of the RMSNorm layer.
-
-        Args:
-            x (torch.Tensor): Input tensor of shape [B, T, d_model].
-
-        Returns:
-            torch.Tensor: Normalized output tensor of same shape.
-        """
-        with torch.amp.autocast(device_type=device.type, dtype=dtype):
-            return self.weight * (
-                x / torch.sqrt(torch.mean(x ** 2, dim=-1, keepdim=True) + self.eps)
-            )
 
 
 class KVCache:
@@ -757,92 +725,7 @@ class Attention(nn.Module):
 
             # Final projection
             return self.w_o(out), cache_out
-
-
-class SwiGLUExpert(nn.Module):
-    """SwiGLU expert layer.
-
-    Args:
-        d_model (int): Dimensionality of the model's embeddings.
-        d_ffn (int): Dimensionality of the feed forward network.
-        dropout (float): Probability of model components being dropped out.
-    """
-    def __init__(self, d_model: int, d_ffn: int, dropout: float):
-        super().__init__()
-
-        self.weight1 = nn.Linear(d_model, d_ffn, bias=False)
-        self.weight2 = nn.Linear(d_model, d_ffn, bias=False)
-        self.weight3 = nn.Linear(d_ffn, d_model, bias=False)
-        self.dropout = nn.Dropout(p=dropout)
-    
-    def _optimized_swiglu(self, x: torch.Tensor) -> torch.Tensor:
-        """Optimized SwiGLU activation using xformers.
         
-        Args:
-            x (torch.Tensor): Input tensor of shape [B, T, d_model].
-            
-        Returns:
-            torch.Tensor: Output tensor of same shape.
-
-        Notes:
-            - We do self.matrix.weight.T as xformers func needs the matrix to be broadcastable
-            with x raw (x @ matrix) (basically, without transpose we cannot apply matrix multiplication
-            due to a shape error).
-            - When we call SwiGLU, all the `None`s being passed, are `Optional[torch.Tensor]`s
-            representing bias.
-            - Requires a contiguous tensor, so we use .contiguous() on the input tensor.
-
-        Requirements:
-            xformers swiglu() import must be succesful.
-            `device` must be cuda.
-            x must live on `device` of cuda.
-            x must have dtype of float16 or bfloat16.
-
-        NOTE: OPTIMIZED SWIGLU HAS NOT BEEN TESTED DUE TO HARDWARE REQUIREMENTS.
-        """
-        # xformers swiglu
-        if (
-            use_xformers_swiglu
-            and device.type == "cuda"
-            and x.is_cuda
-            and x.dtype in [torch.float16, torch.bfloat16]
-        ):
-            return self.dropout(swiglu(
-                x.contiguous(),
-                self.weight1.weight.T, None,
-                self.weight2.weight.T, None,
-                self.weight3.weight.T, None
-            ))
-        # pytorch swiglu fallback
-        else:
-            warnings.warn("xformers SwiGLU not available, falling back to PyTorch SwiGLU.")
-            return self._pytorch_swiglu(x)
-    
-    def _pytorch_swiglu(self, x: torch.Tensor) -> torch.Tensor:
-        """Optimized SwiGLU activation using xformers.
-        
-        Args:
-            x (torch.Tensor): Input tensor of shape [B, T, d_model].
-            
-        Returns:
-            torch.Tensor: Output tensor of same shape.
-        """
-        return self.dropout(self.weight3(F.silu(self.weight1(x)) * self.weight2(x)))
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the SwiGLU Expert layer.
-
-        Formula:
-            SwiGLU(x) = Dropout(Swish((W1 @ x) * (W3 @ x)) @ W2)
-
-        Args:
-            x (torch.Tensor): Input tensor of shape [B, T, d_model].
-
-        Returns:
-            torch.Tensor: Output tensor with the same shape.
-        """
-        with torch.amp.autocast(device_type=device.type, dtype=dtype):
-            return self._optimized_swiglu(x)
 
 class TopKRouter(nn.Module):
     """TopK Routing for MoE layer.
@@ -997,7 +880,9 @@ class MoELayer(nn.Module):
 
         # Experts
         self.experts = nn.ModuleList([
-            SwiGLUExpert(d_model, d_ffn, dropout).to(device) for _ in range(num_experts)
+            SwiGLUActivation(
+                d_model, d_ffn, dropout
+            ).to(device) for _ in range(num_experts)
         ])
 
         # Set up RMSNorm
