4e6b622175f6c26a734a7718d9f04184728d2450
Shaan Vats <shaanv3121@gmail.com>
2025-08-04 16:50:28 -0700

    called super().__post_init__() to call assertions

--- configs/transformers/nlp/model_args/model_args_medium.py
+++ configs/transformers/nlp/model_args/model_args_medium.py
@@ -9,7 +9,7 @@ class ModelArgs(PostInitMixin):
     """Medium configuration of model arguments."""
     d_model: int = 1440
     num_heads: int = 24
-    query_groups: int = 1
+    query_groups: int = 8
     softmax_scale: Optional[float] = None
     d_ffn: int = 5760
     num_layers: int = 20
@@ -25,7 +25,7 @@ class ModelArgs(PostInitMixin):
     use_proj_bias: bool = False
     use_qkv_proj: bool = True
     use_causal: bool = True
-    use_mqa: bool = True
+    use_mqa: bool = False
     use_cache: bool = False
     max_batch_size: int = 1024
     num_experts: int = 1
@@ -35,3 +35,6 @@ class ModelArgs(PostInitMixin):
         """Post initialization to set softmax scale dynamically."""
         if self.softmax_scale is None:
             self.softmax_scale = 1 / math.sqrt(self.d_model // self.num_heads)
+
+        # Call assertions
+        super().__post_init__()
