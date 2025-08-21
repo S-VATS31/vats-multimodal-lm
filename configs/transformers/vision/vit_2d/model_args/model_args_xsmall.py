064540eb0e754458afd175e0488e83624c0bcb1d
Shaan Vats <shaanv3121@gmail.com>
2025-08-20 17:31:38 -0700

    updated model args based on new additions on redone 2d image encoder

--- configs/transformers/vision/vit_2d/model_args/model_args_xsmall.py
+++ configs/transformers/vision/vit_2d/model_args/model_args_xsmall.py
@@ -1,33 +1,32 @@
+import math
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Optional
 
 @dataclass
 class ModelArgs:
     """Extra small configuration of model arguments, containing 89 million parameters."""
-    img_size: int = 384
+    target_size: int = 384
     patch_size: int = 16
     C_in: int = 3
     d_model: int = 768
     num_heads: int = 16
     query_groups: int = 8
+    softmax_scale: Optional[float] = None
     d_ffn: int = 3072
     num_layers: int = 10
-    window_size: Tuple[int, int] = (128, 128)
+    left_window: int = -1
+    right_window: int = -1
     dropout: float = 0.2
-    rope_base: float = 30000.0
+    rope_theta: float = 30000.0
     rms_norm_eps: float = 1e-7
-    num_classes = 1000 # change for different datasets
     use_checkpointing: bool = True
-    
+    use_windowed_attn: bool = True
+    use_proj_bias: bool = False
+    use_fused_proj: bool = True
+    use_mqa: bool = False
+
     def __post_init__(self):
-        """Post initialization for assertions."""
-        if self.d_model % self.num_heads != 0:
-            raise ValueError(f"Expected d_model to be divible by num_heads, got {self.d_model} % {self.num_heads} != 0.")
-        if self.num_heads % self.query_groups != 0:
-            raise ValueError(f"Expected num_heads to be divible by query_groups, got {self.num_heads} % {self.query_groups} != 0.")
-        if self.d_model * 4 != self.d_ffn:
-            raise ValueError(f"Expected d_ffn = d_model * 4, got {self.d_model} * 4 != {self.d_ffn}")
-        if len(self.window_size) != 2:
-            raise ValueError(f"Expected len(window_size) to be equal to 2, got {len(self.window_size)}")
-        if self.window_size[0] != self.window_size[1]:
-            raise ValueError(f"Expected left and right windows to be equal, got {self.window_size[0]} != {self.window_size[1]}")
+        """Post initialization for dynamic softmax scale."""
+        if self.softmax_scale is None:
+            self.softmax_scale = 1 / math.sqrt(self.d_model // self.num_heads)
+            
