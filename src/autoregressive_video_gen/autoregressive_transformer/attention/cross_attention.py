FIXED ALL GRADIENT PROBLEMS BY CONCATENATING SPATIAL + TEMPORAL OUTS; DID FOR CROSS + CAUSAL ATTN

src/autoregressive_video_gen/autoregressive_transformer/attention/cross_attention.py
@@ -62,6 +62,7 @@ class FactorizedCrossAttention(nn.Module):
             use_proj_bias=use_proj_bias, 
             query_groups=query_groups
         )
+        self.spatio_temporal_proj = nn.Linear(2*d_model, d_model, bias=use_proj_bias)
 
     def _optimized_attention(
         self,
@@ -174,7 +175,7 @@ class FactorizedCrossAttention(nn.Module):
             attn_out
             .transpose(1, 2)
             .contiguous()
-            .view(query.size(0), query.size(2), -1)
+            .view(query.size(0), query.size(2), self.d_model)
         )
 
         return attn_out
@@ -281,7 +282,7 @@ class FactorizedCrossAttention(nn.Module):
         ), "expected x.size(-1) == text_embeddings.size(-1)"
 
         # Reshape image tensor to [B*T_frames, H*W, d_model] for spatial attn
-        x = x.view(-1, num_spatial_patches, self.d_model)
+        x = x.view(B*T_frames, num_spatial_patches, self.d_model)
 
         # Project input tensors
         # q: [B*T_frames, H*W, d_model]; k, v shape: [B, T_tokens, query_groups*head_dim]
@@ -368,7 +369,7 @@ class FactorizedCrossAttention(nn.Module):
         ), "x.size(-1) == text_embeddings.size(-1) must be True."
 
         # Reshape x to [B*H*W, T_frames, d_model] for temporal attn
-        x = x.view(-1, T_frames, self.d_model)
+        x = x.view(B*num_spatial_patches, T_frames, self.d_model)
 
         assert (
             x.shape == (B*num_spatial_patches, T_frames, self.d_model)
@@ -456,12 +457,13 @@ class FactorizedCrossAttention(nn.Module):
 
             # [B, T_frames, H*W, d_model]
             spatial_out = spatial_out.view(
-                x.size(0), x.size(1), -1, self.d_model
+                x.size(0), x.size(1), x.size(2), self.d_model
             )
+            spatial_out += x
 
             # [B*H*W, T_frames, d_model]
             temporal_out = self._temporal_cross_attention(
-                spatial_out,
+                x,
                 text_embeddings=text_embeddings,
                 use_mqa=use_mqa,
                 use_qk_norm=use_qk_norm,
@@ -469,14 +471,21 @@ class FactorizedCrossAttention(nn.Module):
             )
 
             # [B, T_frames, H*W, d_model]
-            spatio_temporal_out = temporal_out.view(
-                x.size(0), x.size(1), -1, self.d_model
+            temporal_out = temporal_out.view(
+                x.size(0), x.size(1), x.size(2), self.d_model
             )
+            temporal_out += x
+
+            # [B, T_frames, H*W, 2*d_model]
+            spatio_temporal_out = torch.cat([spatial_out, temporal_out], dim=-1)
+
+            # Project back down to d_model]
+            spatio_temporal_out = self.spatio_temporal_proj(spatio_temporal_out)
 
             return self.o_proj(spatio_temporal_out)
 
 class FactorizedCrossAttentionBlock(nn.Module):
-    """Factorized cross attention layer applying attn, normalizagion, dropout, and residuals.
+    """Factorized cross attention layer applying attn, normalization, dropout, and residuals.
     
     Args:
         d_model (int): Dimensionality of model embeddings.

src/autoregressive_video_gen/autoregressive_transformer/model.py
@@ -10,7 +10,7 @@ from torch.utils.checkpoint import checkpoint
 from src.rms_norm import RMSNorm
 from src.ffn_block import FFNBlock
 from src.optimized_attention import KVCache
-from configs.autoregressive_video_gen.autoregressive_transformer.model_args.model_args_large import ModelArgs
+from configs.autoregressive_video_gen.autoregressive_transformer.model_args.model_args_xsmall import ModelArgs
 from src.autoregressive_video_gen.autoregressive_transformer.attention.cross_attention import FactorizedCrossAttentionBlock
 from src.autoregressive_video_gen.autoregressive_transformer.attention.optimized_attention import CausalFactorizedAttentionBlock
 
@@ -143,7 +143,6 @@ class AutoregressiveVideoTransformerBlock(nn.Module):
 
             return block_out
 
-
 class AutoregressiveVideoTransformer(nn.Module):
     """Autoregressive transformer stacking blocks.
     
@@ -197,7 +196,7 @@ class AutoregressiveVideoTransformer(nn.Module):
         )
 
         # Initialize weights
-        # self.apply(self._init_weights)
+        self.apply(self._init_weights)
 
     def _init_weights(self, module) -> None:
         """Initialize weights based on given module.
@@ -205,11 +204,19 @@ class AutoregressiveVideoTransformer(nn.Module):
         Args:
             module: Module to be initialized.
         """
-        pass
+        if isinstance(module, nn.Linear) or isinstance(module, nn.Embedding):
+            nn.init.xavier_uniform_(module.weight)
+            if hasattr(module, 'bias') and module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif isinstance(module, (nn.LayerNorm, RMSNorm)):
+            if hasattr(module, 'weight') and module.weight is not None:
+                nn.init.ones_(module.weight)
+            if hasattr(module, 'bias') and module.bias is not None:
+                nn.init.zeros_(module.bias)
 
     def forward(
         self,
-        x: torch.Tensor,
+        x: torch.LongTensor,
         text_embeddings: torch.Tensor,
         use_cache: bool,
         spatio_temoral_padding_mask: Optional[torch.Tensor] = None,
@@ -229,18 +236,23 @@ class AutoregressiveVideoTransformer(nn.Module):
         """
         assert x.dim() == 4, f"expected 4 dims, got {x.dim()} dims"
         assert text_embeddings.dim() == 3, f"expected 3 dims, got {text_embeddings.dim()} dims"
-        assert (
-            spatio_temoral_padding_mask.dim() == 2
-        ), f"expected 2 dims, got {spatio_temoral_padding_mask.dim()} dims"
-        assert (
-            text_padding_mask.dim() == 2
-        ), f"expected 2 dims, got {text_padding_mask.dim()} dims"
-        assert (
-            spatio_temoral_padding_mask.shape == (x.size(0), x.size(1)*x.size(2)*x.size(3))
-        ), f"expected {(x.size(0), x.size(1)*x.size(2)*x.size(3))}, got {spatio_temoral_padding_mask.shape}"
-        assert (
-            text_padding_mask.shape == (text_embeddings.size(0), text_embeddings.size(1))
-        ), f"expected {(text_embeddings.size(0), text_embeddings.size(1))}, got {text_padding_mask.shape}"
+        if spatio_temoral_padding_mask is not None:
+            assert (
+                spatio_temoral_padding_mask.dim() == 2
+            ), f"expected 2 dims, got {spatio_temoral_padding_mask.dim()} dims"
+            assert (
+                spatio_temoral_padding_mask.shape == (x.size(0), x.size(1)*x.size(2)*x.size(3))
+            ), f"expected {(x.size(0), x.size(1)*x.size(2)*x.size(3))}, got {spatio_temoral_padding_mask.shape}"
+        if text_padding_mask is not None:
+            assert (
+                text_padding_mask.dim() == 2
+            ), f"expected 2 dims, got {text_padding_mask.dim()} dims"
+            assert (
+                text_padding_mask.shape == (text_embeddings.size(0), text_embeddings.size(1))
+            ), f"expected {(text_embeddings.size(0), text_embeddings.size(1))}, got {text_padding_mask.shape}"
+
+        if x.dtype != torch.int64:
+            x = x.to(torch.int64)
 
         # [B, T_frames, H*W, d_model]
         x = self.dropout(self.embedding(x))
@@ -251,7 +263,7 @@ class AutoregressiveVideoTransformer(nn.Module):
 
         B, T_frames, H, W, _ = x.shape # will be used for reshaping later
 
-        x = x.view(x.size(0), x.size(1), -1, x.size(-1))
+        x = x.view(x.size(0), x.size(1), H*W, x.size(-1))
         assert (
             x.shape == (B, T_frames, H*W, self.model_args.d_model)
         ), f"expected {(B, T_frames, H*W, self.model_args.d_model)}, got {x.shape}"
@@ -285,24 +297,25 @@ class AutoregressiveVideoTransformer(nn.Module):
                     left_window=self.model_args.left_window,
                     right_window=self.model_args.right_window,
                     use_cache=use_cache,
-                    spatio_temoral_padding_mask=spatio_temoral_padding_mask,
+                    spatio_temporal_padding_mask=spatio_temoral_padding_mask,
                     text_padding_mask=text_padding_mask,
                     kv_cache=self.kv_cache,
                     layer_idx=layer_idx
                 )
+                
 
         assert x.dim() == 4, f"expected 4 dims, got {x.dim()} dims"
 
         # Apply RMSNorm
         x = self.rms_norm(x)
 
-        return x.view(B, T_frames, H, W, -1)  # [B, T, H, W, d_model]
+        return x.view(B, T_frames, H, W, self.model_args.d_model)  # [B, T, H, W, d_model]
 
 def test_transformer_block():
-    d_model, num_heads, query_groups, rope_theta = 512, 32, 8, 10000.0
-    softmax_scale, d_ffn = (d_model // num_heads) ** 0.5, 4*d_model
+    d_model, num_heads, query_groups, rope_theta = 128, 16, 4, 5000.0
+    softmax_scale, d_ffn = 1/((d_model // num_heads) ** 0.5), 4*d_model
     use_proj_bias, use_fused_proj, use_windowed_attn = False, True, True
-    use_ntk_rope, eps, dropout, ntk_scale_factor = True, 1e-12, 0.15, 0.7
+    use_ntk_rope, eps, dropout, ntk_scale_factor = True, 1e-5, 0.15, 0.7
     transformer_block = AutoregressiveVideoTransformerBlock(
         d_model, num_heads, query_groups,
         rope_theta, softmax_scale, use_proj_bias,
@@ -326,7 +339,7 @@ def test_transformer_block():
         head_dim=d_model//num_heads,
         num_layers=4
     )
-    x_out = transformer_block(
+    out = transformer_block(
         x,
         text_embeddings=text_embeddings,
         use_mqa=False,
@@ -340,19 +353,17 @@ def test_transformer_block():
         kv_cache=kv_cache,
         layer_idx=1
     )
-    return x_out
+    loss = out.sum()
+    loss.backward()
+    for name, param in transformer_block.named_parameters():
+        print(f"{name}: {param.grad.isnan().any()}")
 
 def test_transformer_forward():
     model_args = ModelArgs()
     model = AutoregressiveVideoTransformer(model_args).to(device)
-    B, T_frames, H, W = 1, 15, 32, 32
-    T_tokens = 16
-    x = torch.randint(
-        low=0, 
-        high=model_args.num_embeddings,
-        size=(B, T_frames, H, W),
-        device=device
-    )
+    B, T_frames, H, W = 1, 2, 8, 8
+    T_tokens = 3
+    x = torch.randint(0, model_args.num_embeddings, (B, T_frames, H, W),device=device)
     text_embeddings = torch.randn(B, T_tokens, model_args.d_model).to(device)
     spatio_temporal_padding_mask = torch.randint(
         0, 2, (B, T_frames*H*W), dtype=torch.bool
@@ -360,16 +371,18 @@ def test_transformer_forward():
     text_padding_mask = torch.randint(
         0, 2, (B, T_tokens), dtype=torch.bool
     ).to(device)
-    x_out = model(
+    out: torch.Tensor = model(
         x,
         text_embeddings=text_embeddings,
-        use_cache=True,
+        use_cache=False,
         spatio_temoral_padding_mask=spatio_temporal_padding_mask,
         text_padding_mask=text_padding_mask
     )
-    return x_out
-
+    loss = out.float().sum()
+    loss.backward()
+    for name, param in model.named_parameters():
+        print(f"{name}: {param.grad}")
+    return out
 
 if __name__ == "__main__":
-    x = test_transformer_forward()
-    print(x.shape)
+    test_transformer_forward()

src/rms_norm.py
@@ -1,8 +1,7 @@
-from configs.setup_env import device, dtype
+from configs.setup_env import device
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.amp import autocast
 
 class RMSNorm(nn.Module):
@@ -28,8 +27,11 @@ class RMSNorm(nn.Module):
         Args:
             x (torch.Tensor): Input tensor of shape [B, T, d_model].    
         """
-        with autocast(device_type=device.type, dtype=dtype):
+        with autocast(device_type=device.type, enabled=False):
+            assert (
+                x.size(-1) == self.d_model
+            ), f"expected {self.d_model}, got {x.size(-1)}"
             return self.weight * (
-                F.normalize(x, p=2, dim=-1)
+                x / torch.sqrt(torch.mean(x**2, keepdim=True, dim=-1) + self.eps)
             )
         

tests/autoregressive_video_gen/model_tests.py
@@ -10,39 +10,123 @@ from configs.autoregressive_video_gen.autoregressive_transformer.model_args.mode
 model_args = ModelArgs()
 
 def setup():
-    pass
+    model = AutoregressiveVideoTransformer(model_args).to(device)
+    B, T_frames, H, W = 1, 8, 16, 16
+    T_tokens = 12
+    x = torch.randint(
+        0, model_args.num_embeddings, (B, T_frames, H, W), dtype=torch.int64
+    ).to(device)
+    text_embeddings = torch.randn(B, T_tokens, model_args.d_model).to(device)
+    image_padding_mask = torch.randint(
+        0, 2, (B, T_frames*H*W), dtype=torch.bool
+    ).to(device)
+    text_padding_mask = torch.randint(
+        0, 2, (B, T_tokens), dtype=torch.bool
+    ).to(device)
+    d_model = model_args.d_model
+    num_heads = model_args.num_heads
+    head_dim = d_model // num_heads
+    return (
+        model, 
+        B, T_frames, T_tokens, H, W, 
+        x, text_embeddings, 
+        image_padding_mask, text_padding_mask, 
+        d_model, num_heads, head_dim
+    )
+
+(
+    model, 
+    B, T_frames, T_tokens, H, W, 
+    x, text_embeddings, 
+    image_padding_mask, text_padding_mask, 
+    d_model, num_heads, head_dim
+) = setup()
 
 def test_output_shape():
-    pass
+    out = model(
+        x, text_embeddings, False, image_padding_mask, text_padding_mask
+    )
+    assert out.shape == (B, T_frames, H, W, d_model)
+    print("PASSED OUTPUT SHAPE TEST")
 
 def test_image_padding():
-    pass
+    out = model(
+        x, text_embeddings, False, image_padding_mask, None
+    )
+    assert out.shape == (B, T_frames, H, W, d_model)
+    print("PASSED IMAGE PADDING TEST")
 
 def test_text_padding():
-    pass
+    out = model(
+        x, text_embeddings, False, None, text_padding_mask
+    )
+    assert out.shape == (B, T_frames, H, W, d_model)
+    print("PASSED TEXT PADDING TEST")
 
 def test_combined_padding():
-    pass
+    out = model(
+        x, text_embeddings, False, image_padding_mask, text_padding_mask
+    )
+    assert out.shape == (B, T_frames, H, W, d_model)
+    print("PASSED COMBINED PADDING TEST")
 
 def test_no_padding():
-    pass
+    out = model(
+        x, text_embeddings, False, None, None
+    )
+    assert out.shape == (B, T_frames, H, W, d_model)
+    print("PASSED NO PADDING TEST")
 
 def test_cache():
     pass
 
-def test_no_cache():
-    pass
-
 def test_zero_frames():
-    pass
+    frames = 0
+    x = torch.randint(
+        0, model_args.num_embeddings, (B, frames, H, W), dtype=torch.int64
+    ).to(device)
+    image_mask = torch.randint(
+        0, 2, (B, frames*H*W), dtype=torch.bool
+    ).to(device)
+    out = model(
+        x, text_embeddings, False, image_mask, None
+    )
+    assert out.shape == (B, frames, H, W, d_model)
+    print("PASSED ZERO FRAMES TEST")
 
 def test_zero_tokens():
-    pass
+    tokens = 0
+    text_embeddings = torch.randn(B, tokens, d_model).to(device)
+    text_mask = torch.randint(0, 2, (B, tokens), dtype=torch.bool).to(device)
+    out = model(
+        x, text_embeddings, False, None, text_mask
+    )
+    assert out.shape == (B, T_frames, H, W, d_model)
+    print("PASSED ZERO TOKENS TEST")
+
+def test_zero_frames_zero_tokens():
+    frames, tokens = 0, 0
+    x = torch.randint(
+        0, 2, (B, frames, H, W), dtype=torch.bool
+    ).to(device)
+    image_mask = torch.randint(
+        0, 2, (B, frames*H*W), dtype=torch.bool
+    ).to(device)
+    text_embeddings = torch.randn(B, tokens, d_model).to(device)
+    text_mask = torch.randint(0, 2, (B, tokens), dtype=torch.bool).to(device)
+    out = model(
+        x, text_embeddings, False, image_mask, text_mask
+    )
+    assert out.shape == (B, frames, H, W, d_model)
+    print("PASSED ZERO FRAMES AND TOKENS TEST")
 
 def test_exceeding_max_frames():
     pass
 
 def test_exceeding_max_tokens():
+    pass
+
+def test_exceeding_max_frames_max_tokens():
     pass
 
 def test_square_input_hw():
@@ -52,10 +136,22 @@ def test_non_square_input_hw():
     pass
 
 def test_gradients():
-    pass
-
-def test_weight_shapes():
-    pass
+    x_image = torch.randint()
+    out = model(
+        x, text_embeddings, False, image_padding_mask, text_padding_mask
+    )
+    loss = out.sum()
+    loss.backward()
+    for _, param in model.named_parameters():
+        print(param.grad)
+        assert torch.all(torch.isfinite(param.grad))
+        assert torch.all(torch.isreal(param.grad))
+        assert torch.all(torch.isfinite(param.grad.norm()))
+        assert torch.all(torch.isreal(param.grad.norm()))
+        assert not torch.all(torch.isnan(param.grad))
+        assert not torch.all(torch.isinf(param.grad))
+        assert not torch.all(torch.isnan(param.grad.norm()))
+        assert not torch.all(torch.isinf(param.grad.norm()))
 
 def test_numerical_stability():
     pass
@@ -79,15 +175,15 @@ if __name__ == "__main__":
     test_combined_padding()
     test_no_padding()
     test_cache()
-    test_no_cache()
     test_zero_frames()
     test_zero_tokens()
+    test_zero_frames_zero_tokens()
     test_exceeding_max_frames()
     test_exceeding_max_tokens()
+    test_exceeding_max_frames_max_tokens()
     test_square_input_hw()
     test_non_square_input_hw()
     test_gradients()
-    test_weight_shapes()
     test_numerical_stability()
     test_variable_resolution()
     test_variable_batch_sizes()