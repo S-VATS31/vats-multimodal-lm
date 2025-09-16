e1477f71c90f55e36ce3e853b52628c49fe71ac9
Shaan Vats <shaanv3121@gmail.com>
2025-09-15 20:40:46 -0700

    fixed input_ids for imagegen model

--- src/autoregressive_image_gen/vq_vae/decoder/vae_decoder.py
+++ src/autoregressive_image_gen/vq_vae/decoder/vae_decoder.py
@@ -79,31 +79,19 @@ class VQVAEDecoder(nn.Module):
         """Forward pass of Decoder layer.
         
         Args:
-            x (torch.Tensor): Input tensor of shape [B, H*W, d_model].
+            x (torch.Tensor): Input tensor of shape [B, H, W, d_model].
 
         Returns:
             torch.Tensor: Output tensor of shape [B, C_out, H, W].
         """
         with autocast(device_type=device.type, dtype=dtype):
-            assert x.dim() == 3, f"expected 3 dims, got {x.dim()} dims."
-            assert x.size(-1) == self.d_model, f"expected {self.d_model}, got {x.size(-1)}"
-            
-            # Reshape x to [B, d_model, H, W]
-            B, num_spatial_patches, _ = x.shape
-            H = W = math.isqrt(num_spatial_patches)
-            assert (
-                H * H == num_spatial_patches and
-                W * W == num_spatial_patches
-            ), f"num_spatial_patches must be a perfect square, got {num_spatial_patches}"
+            assert x.dim() == 4, f"expected 4 dims, got {x.dim()} dims."
+            B, H, W, _ = x.shape
+            assert x.size(-1) == self.d_model, f"expected last dim {self.d_model}, got {x.size(-1)}"
 
-            x = (
-                x.transpose(1, 2) # [B, d_model, H*W]
-                .contiguous()
-                .view(B, self.d_model, H, W)
-            ) # [B, d_model, H, W]
-            assert (
-                x.shape == (B, self.d_model, H, W)
-            ), f"expected: {(B, self.d_model, H, W)}, got {x.shape}"
+            # Reshape to [B, d_model, H, W] for ConvTranspose2d
+            x = x.permute(0, 3, 1, 2).contiguous()
+            assert x.shape == (B, self.d_model, H, W), f"expected {(B, self.d_model, H, W)}, got {x.shape}"
 
             # Pass through deconvolution blocks
             x = self.activation(self.batch_norm1(self.deconv1(x)))
@@ -112,7 +100,7 @@ class VQVAEDecoder(nn.Module):
             # Final deconvolution block
             x = self.sigmoid(self.deconv3(x)) # [B, C_out, H, W]
 
-            assert x.dim() == 4, f"expected 3 dims, got {x.dim()} dims"
+            assert x.dim() == 4, f"expected 4 dims, got {x.dim()} dims"
             assert x.size(1) == self.C_out, f"expected {self.C_out}, got {x.size(1)}"
 
             return x
@@ -121,11 +109,10 @@ def test_decoder():
     C_out, d_model = 3, 512
     decoder = VQVAEDecoder(C_out, d_model, "relu").to(device)
     B, H, W = 1, 144, 144
-    x = torch.randn(B, H*W, d_model).to(device)
+    x = torch.randn(B, H, W, d_model).to(device)
     x_out = decoder(x)
     return x_out
 
 if __name__ == "__main__":
     x = test_decoder()
     print(x.shape)
-    
--- src/autoregressive_image_gen/vq_vae/vq_vae.py
+++ src/autoregressive_image_gen/vq_vae/vq_vae.py
@@ -15,21 +15,18 @@ class VQVAE(nn.Module):
     def __init__(self, model_args: ModelArgs):
         super().__init__()
 
-        # Encoder
         self.encoder = VQVAEEncoder(
             C_in=model_args.C_in_out,
             d_model=model_args.d_model,
             activation_func=model_args.vae_encoder_activation
         ).to(device)
 
-        # Quantizer
         self.quantizer = VectorQuantizer(
             d_model=model_args.d_model,
             num_embeddings=model_args.num_embeddings,
             commitment_beta=model_args.commitment_beta
         ).to(device)
 
-        # Decoder
         self.decoder = VQVAEDecoder(
             C_out=model_args.C_in_out,
             d_model=model_args.d_model,
